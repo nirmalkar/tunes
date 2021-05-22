5affb07fffbac2ef6ba181b391bfb07efde93fd6
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-05-22 10:49:31 +0530

    copy changes

--- src/constants/EntertainmentCards/index.js
+++ src/constants/EntertainmentCards/index.js
@@ -6,14 +6,14 @@ const entertainmentCards = [
     type: "Radio",
     image: music,
     description:
-      "It's amazing to have choices when it comes to listen radio, we more than 5000 channels available with all kind music genre.",
+      "It's amazing to have choices when it comes to listen radio, we have more than 5000 channels available with all kind music genre.",
     link: "/radio",
   },
   {
     type: "Podcast",
     image: podcast,
     description:
-      "Getting bored or wanna be update with all the information of your choice, Here are the best podcasts out there on the planet.",
+      "Getting bored or wanna be update with all the information of your choices, Here are the best podcasts out there on the planet.",
     link: "/podcast",
   },
 ];
