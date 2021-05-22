7cbd3d50e9a31ac5c9cfaf8154ea78272c1dfeb5
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-05-22 22:12:54 +0530

    created auth component

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 
 import Home from "containers/Home";
 import Radio from "containers/Radio";
+import Auth from "containers/Auth";
 
 const Routes = () => {
   return (
@@ -10,6 +11,7 @@ const Routes = () => {
       <Route exact path="/" component={Home} />
       <Route exact path="/radio" component={Radio} />
       <Route exact path="/podcast" component={Radio} />
+      <Route exact path="/auth" component={Auth} />
     </div>
   );
 };
